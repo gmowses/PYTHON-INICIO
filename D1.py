0567946bdc98d3133be37ed8e73d02f3db110771
Gabriel Mowses <39505233+gmowses@users.noreply.github.com>
2022-01-17 10:36:43 -0300

    Update D1.py

--- D1.py
+++ D1.py
@@ -1,5 +1,5 @@
 from logging.handlers import RotatingFileHandler
 
 
-nome = input('Qual é o seu nome?')
-print('Olá',nome, '!' 'Seja Bem Vindo')
+nome = input('Qual é o seu nome? ')
+print('Olá',nome, '! ' 'Seja Bem Vindo !')
