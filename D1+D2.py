a8ca782fd89db907b76fbdc2232bdec46931d4d9
Rafael-Mowses <rafaelmowsestec@gmail.com>
2022-01-17 13:19:49 -0300

    Desafio1+2

---
+++ D1+D2.py
@@ -0,0 +1,7 @@
+nome = input('Qual seu nome?')
+print('Olá',nome, 'Nos informe a data do seu nascimento')
+dia = input('Qual Dia você nasceu ? ')
+mes = input('Qual Mês você nasceu ? ')
+ano = input('Qual Ano você nasceu ? ')
+print('Olá',nome, 'Seja Bem Vindo, Você nasceu no dia',dia,
+      'de',mes, 'de',ano, '. Correto?')
