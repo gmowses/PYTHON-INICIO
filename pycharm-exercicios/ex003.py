91ae1a9f443ca81c66347e6c9d5feb3dfd740b0d
Gabriel Mowses <39505233+gmowses@users.noreply.github.com>
2022-01-18 14:45:03 -0300

    Update ex003.py

--- pycharm-exercicios/ex003.py
+++ pycharm-exercicios/ex003.py
@@ -1,5 +1,5 @@
 print('========CALCULADORA DE ADIÇÃO========')
-n1 =int(input('Digite um número: '))
-n2 =int(input('Digite mais um número: '))
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
 s = n1+n2
 print('A Soma vale',s)
